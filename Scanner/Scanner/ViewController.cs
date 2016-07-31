862ca52a1705274d67e24fd2fca616aa7f50b119
Jake Wong <jakewongjp@gmail.com>
2016-07-31 11:29:31 -0400

    up

---
+++ Scanner/Scanner/ViewController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace Scanner
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			// Second method----------------------------------
+			// Need to create: void ClickABC(object sender, EventArgs e)
+			// GO TO DECLARATION TO KNOW THE SIGNATURE
+			abc.TouchDown += ClickABC;
+
+			efg.TouchDown += (object sender, EventArgs e) =>
+			{
+
+				var tt = new UIAlertView("fgf", "fgdgfdfd", new UIAlertViewDelegate(), "b1", "b2", "b3");
+
+
+
+				tt.Show();
+
+			};
+
+
+
+
+
+			xxx.TouchDown += (object sender, EventArgs e) => {
+
+				nint result = 0;
+				var alert = new UIAlertView
+				{
+					Title = "Title",
+					Message = "MESSAGE"
+				};
+
+				alert.AddButton("OK");
+				alert.AddButton("CANCEL");
+
+
+				alert.Clicked += (s2, e2) => {
+
+
+
+				
+						outputlabel.Text = e2.ButtonIndex.ToString();
+				
+				
+				};
+
+
+				alert.Show();
+
+
+			};
+
+
+
+
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+
+		void ClickABC(object sender, EventArgs e)
+		{
+			UIAlertView tt = new UIAlertView();
+			tt.Title = "ABC";
+			tt.Show();
+
+		}
+
+
+
+		// we used ACTION (iAction) the outlet method and print
+		partial void ButtonCLick3(Foundation.NSObject sender)
+		{
+			UIAlertView tt = new UIAlertView();
+			tt.Title = "test";
+			tt.Show();
+		}
+
+
+
+
+
+
+
+
+	}
+}
+
---
+++ Scanner/Scanner/ViewController.designer.cs
@@ -0,0 +1,61 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Scanner
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UIButton abc { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnScan { get; set; }
+
+		[Outlet]
+		UIKit.UIButton efg { get; set; }
+
+		[Outlet]
+		UIKit.UILabel outputlabel { get; set; }
+
+		[Outlet]
+		UIKit.UIButton xxx { get; set; }
+
+		[Action ("ButtonCLick3:")]
+		partial void ButtonCLick3 (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (btnScan != null) {
+				btnScan.Dispose ();
+				btnScan = null;
+			}
+
+			if (abc != null) {
+				abc.Dispose ();
+				abc = null;
+			}
+
+			if (efg != null) {
+				efg.Dispose ();
+				efg = null;
+			}
+
+			if (xxx != null) {
+				xxx.Dispose ();
+				xxx = null;
+			}
+
+			if (outputlabel != null) {
+				outputlabel.Dispose ();
+				outputlabel = null;
+			}
+		}
+	}
+}
